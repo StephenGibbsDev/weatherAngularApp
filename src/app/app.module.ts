b43b0f12a1f881d4d55af5f6720a09d30a361411
Stephen Gibbs <stephenbandung@hotmail.co.uk>
2019-08-10 16:32:06 +0100

    got bootstrap working and embedded components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule} from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { WeatherLandingComponent } from './weather-landing/weather-landing.component';
+import { WeatherHeaderComponent } from './weather-landing/weather-header/weather-header.component';
+import { WeatherDetailsComponent } from './weather-landing/weather-details/weather-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherLandingComponent,
+    WeatherHeaderComponent,
+    WeatherDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/weather-landing/weather-landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-landing',
+  templateUrl: './weather-landing.component.html',
+  styleUrls: ['./weather-landing.component.css']
+})
+export class WeatherLandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
