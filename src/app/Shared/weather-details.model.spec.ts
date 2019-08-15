ad2419655b72ae3de2aa2593ff41793634c8b9a7
Stephen Gibbs <stephenbandung@hotmail.co.uk>
2019-08-15 16:28:50 +0100

    Auto-generated commit

--- src/app/Shared/weather-details.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { WeatherDetails } from './weather-details.model';
-
-describe('WeatherDetails', () => {
-  it('should create an instance', () => {
-    expect(new WeatherDetails()).toBeTruthy();
-  });
-});
--- src/app/Shared/weather-details.service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { WeatherDetails } from './weather-details.service';
-
-describe('WeatherDetails', () => {
-  it('should create an instance', () => {
-    expect(new WeatherDetails()).toBeTruthy();
-  });
-});
